963f88606e124517ee445afae95a97ffdddf911d
Denis Cheruiyot <Cheruiyotkp974@gmail.com>
2025-04-15 21:30:19 +0300

    expense

--- development/phase-2/expense-tracker/src/App.jsx
+++ development/phase-2/expense-tracker/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseTable from "./components/ExpenseTable";
+import SearchBar from "./components/SearchBar";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [expenses, setExpenses] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const addExpense = (newExpense) => {
+    setExpenses([newExpense, ...expenses]);
+  };
+
+  const deleteExpense = (id) => {
+    setExpenses(expenses.filter((e) => e.id !== id));
+  };
+
+  const filteredExpenses = expenses.filter((expense) => {
+    const keyword = search.toLowerCase();
+    return (
+      expense.name.toLowerCase().includes(keyword) ||
+      expense.description.toLowerCase().includes(keyword) ||
+      expense.category.toLowerCase().includes(keyword) ||
+      expense.date.includes(keyword)
+    );
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="container">
+      <h1 className="title">Expense Tracker</h1>
+      <p className="subtitle">
+        Use this tracker to start monitoring your Expenses.
       </p>
-    </>
-  )
-}
+      <div className="main">
+        <div className="left">
+          <ExpenseForm onAddExpense={addExpense} />
+        </div>
+        <div className="right">
+          <SearchBar search={search} setSearch={setSearch} />
+          <ExpenseTable expenses={filteredExpenses} onDelete={deleteExpense} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
